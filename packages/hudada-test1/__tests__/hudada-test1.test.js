5d57975fa2360dac1321870611a3b2f01d76d64a
humingxian <hudadacool@163.com>
2020-10-30 15:31:04 +0800

    clean test readme

--- packages/hudada-test1/__tests__/hudada-test1.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const hudadaTest1 = require('..');
-
-describe('hudada-test1', () => {
-    it('needs tests');
-});
--- packages/hudada-test2/__tests__/hudada-test2.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const hudadaTest2 = require('..');
-
-describe('hudada-test2', () => {
-    it('needs tests');
-});
